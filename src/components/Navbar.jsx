bba7d65022b98750deb4591ff2846a8cf8b472d6
proPain0719 <97197314+RylesAnderson@users.noreply.github.com>
2025-10-14 14:22:46 -0400

    Created a navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+import "./Navbar.css";
+import { Link, NavLink } from "react-router-dom";
+
+export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <nav className="tool-bar-navigation">
+      <Link to="/" className="title">
+        Spotify API Tutorial
+      </Link>
+      <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <ul className={menuOpen ? "open" : ""}>
+        <li>
+          <NavLink to="/MusicBoardSearch">Music Search Board</NavLink>
+        </li>
+        <li>
+          <NavLink to="/services">Services</NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact">Contact</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
